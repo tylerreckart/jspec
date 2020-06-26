241002f5e89efb3e25d40469b7aed593ab32fbae
Tyler Reckart <tyler@alley.co>
2020-06-26 06:22:52 -0400

    add comments

--- src/runner.js
+++ src/runner.js
@@ -3,7 +3,6 @@ const chalk = require('chalk');
 let testCount = 0;
 let passCount = 0;
 let failCount = 0;
-
 let activeSuite = { it: [] };
 let activeTest = {};
 
@@ -12,24 +11,27 @@ const results = [];
 const log = console.log;
 
 const describe = (desc, callback) => {
+  // Reset the active suite.
   activeSuite = { it: [] };
+  // Apply test constants.
   activeSuite.name = desc;
-
+  // Run any `it` blocks.
   callback.apply(this);
-
+  // Push the active suite to the results array.
   results.push(activeSuite);
 };
 
 const it = (desc, callback) => {
+  // Increment the test count.
   testCount++;
-
+  // Set the active test.
   activeTest = {
     name: desc,
     expects: [],
   };
-
+  // Run the callback.
   callback.apply(this);
-
+  // Add the test to the active suite.
   activeSuite.it.push(activeTest);
 };
 
@@ -39,12 +41,14 @@ const formatResult = (value, method, expected) =>
 const expect = (value) => ({
   toEqual: (expected) => {
     if (value === expected) {
+      // The test passed. Log the result.
       activeTest.expects.push({
         name: formatResult(value, 'toEqual', expected),
         pass: true,
       });
       passCount++;
     } else {
+      // The test failed. Log the result.
       activeTest.expects.push({
         name: formatResult(value, 'toEqual', expected),
         pass: false,
